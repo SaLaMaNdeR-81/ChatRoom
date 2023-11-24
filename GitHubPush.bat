8ec1ac7b447481a84b2656eba1f08681bcee03e4
Salamander <a.mahdimahdi1381@gmail.com>
2023-11-24 06:22:40 +0330

    UpDate Project.

--- GitHubPush.bat
+++ GitHubPush.bat
@@ -1,12 +1,12 @@
 @echo off
 Title GitPusher
 
-  @REM git init
-  @REM git add README.md
-  @REM git commit -m "first commit"
-  @REM git branch -M main
-  @REM git remote add origin https://github.com/SaLaMaNdeR-81/ChatRoom.git
-  @REM git push -u origin main
+@REM git init
+@REM git add README.md
+@REM git commit -m "first commit"
+@REM git branch -M main
+@REM git remote add origin https://github.com/SaLaMaNdeR-81/ChatRoom.git
+@REM git push -u origin main
 
 git add . 
 git add .gitignore
